9d8d9eac4be63f10447e841cb25a1f7fd37dd7ad
merveozalpsupurgeci <92928158+merveozalpsupurgeci@users.noreply.github.com>
2023-12-23 02:27:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int min=0, max=0, number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int again = input.nextInt();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for(int count =1;count<=again;count++){
+            
+            System.out.print(count + ". Sayıyı giriniz: ");
+            number = input.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            if (count == 1){
+                max = number;
+                min = number;
+            }
+            else{
+                if(number < min){
+                    min = number;
+                }
+                else if(number > max){
+                    max = number;
+                }
+            }
+
         }
+        System.out.println("Girilen en küçük sayı: " + min);
+        System.out.println("Girilen en büyük sayı: " + max);
     }
 }
